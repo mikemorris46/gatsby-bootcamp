e2c8e838347f30ec1eaeaf514bce2156eff40cbd
mikemorris46 <mike.morris.wa@gmail.com>
2019-04-30 16:43:56 +0800

    Minor change to Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ const IndexPage = () => {
       <Head title='Home' />
       <h1>Hello from Gatsby IndexPage</h1>
       <h2>I'm Mike and I'm retired</h2>
-      <p>
-        Need a Developer? <Link to='/contact'>Contact Me</Link>
-      </p>
     </Layout>
   )
 }
